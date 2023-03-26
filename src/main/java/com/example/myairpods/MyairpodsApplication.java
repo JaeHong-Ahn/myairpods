88610656e64caa92113c4ed83f45f2daa201cc6c
JaeHong-Ahn <woghd387@kyonggi.ac.kr>
2023-03-26 18:41:29 +0900

    Where's my Airpods InitialIze
    에어팟 분실 신고 프로젝트 시작

---
+++ src/main/java/com/example/myairpods/MyairpodsApplication.java
@@ -0,0 +1,13 @@
+package com.example.myairpods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyairpodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyairpodsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myairpods/MyairpodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myairpods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyairpodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
